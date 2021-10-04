cf81a305f9f361554f247b3c2f7569699d567793
kangae <89259630+kangae@users.noreply.github.com>
2021-10-04 12:05:16 +0000

    logo change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![img](https://cdn.mee6.xyz/guild-images/831008344696422441/f66b66bb5ac0a68c5f307a12f10c250cea0b85ed7dbf263f3a5549a9bd3bd34d.jpeg)
+![img](https://cdn.discordapp.com/attachments/850362827255709746/890431887959547964/PG_logo.png)
 
 # Gabut Bot
 Made With 💖 For Pelajar Gabut
--- identity.js
+++ identity.js
@@ -5,7 +5,7 @@ exports.identity = {
   name: 'Gabut Bot',
   status: 'idle',
   prefix: 'g!',
-  thumbnail: 'https://cdn.mee6.xyz/guild-images/831008344696422441/f66b66bb5ac0a68c5f307a12f10c250cea0b85ed7dbf263f3a5549a9bd3bd34d.jpeg',
+  thumbnail: 'https://cdn.discordapp.com/attachments/850362827255709746/890431887959547964/PG_logo.png',
   package: JSON.parse(
     fs.readFileSync(path.join(__dirname, 'package.json'))
   ),
